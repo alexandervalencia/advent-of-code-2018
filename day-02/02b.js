1840a0704fbc0452cf391787fc8f39dfe705d40a
Alexander Valencia <alexander.valencia@liferay.com>
2018-11-30 22:45:29 -0800

    Day 02 - Prepwork

---
+++ day-02/02b.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const input = fs.readFileSync('./input.txt', 'utf8')
+
+console.log(`*** Test 1 ***\nActual Result: ${}\nExpected Result: \n`)
+console.log(`*** Test 2 ***\nActual Result: ${}\nExpected Result: \n`)
+console.log(`*** Test 3 ***\nActual Result: ${}\nExpected Result: \n`)
+console.log(`*** Test 4 ***\nActual Result: ${}\nExpected Result: \n`)
+console.log(`*** Final Test ***\n${}\n`)
